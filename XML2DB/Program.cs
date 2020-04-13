f8e817e3a946a679a8b506aed1d38564c1ad6aa3
Mohamed MERZAK <MERZAK_MOHAMED@emsi-edu.ma>
2020-04-13 23:49:14 +0100

    Created project files

---
+++ XML2DB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XML2DB
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("TEST");
+        }
+    }
+}
---
+++ XML2DB/src/XML/XMLParser.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System.Xml;
+
+namespace XML2DB.XML
+{
+    public static class XMLParser
+    {
+        static bool checkValidXML(XmlDocument xmlFile)
+        {
+            return false;
+        }
+        
+        
+        
+    }
+}
