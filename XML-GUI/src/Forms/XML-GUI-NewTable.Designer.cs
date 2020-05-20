d5d20a8fe024b50f36166b490e3c61d05cd87893
Mohamed MERZAK <merzak_mohamed@emsi-edu.ma>
2020-05-20 04:36:26 +0000

    Local test #40

---
+++ XML-GUI/src/Forms/XML-GUI-NewTable.Designer.cs
@@ -0,0 +1,152 @@
+using System.ComponentModel;
+
+namespace XML_GUI
+{
+    partial class XML_GUI_NewTable
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XML_GUI_NewTable));
+            this.columnsList = new System.Windows.Forms.ListBox();
+            this.columnName = new System.Windows.Forms.TextBox();
+            this.addColumn = new System.Windows.Forms.Button();
+            this.txtClmnName = new System.Windows.Forms.Label();
+            this.btnDone = new System.Windows.Forms.Button();
+            this.deleteColumn = new System.Windows.Forms.Button();
+            this.txtEntityname = new System.Windows.Forms.Label();
+            this.entityName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // columnsList
+            // 
+            this.columnsList.FormattingEnabled = true;
+            this.columnsList.ItemHeight = 20;
+            this.columnsList.Location = new System.Drawing.Point(67, 123);
+            this.columnsList.Name = "columnsList";
+            this.columnsList.Size = new System.Drawing.Size(254, 164);
+            this.columnsList.TabIndex = 5;
+            // 
+            // columnName
+            // 
+            this.columnName.Location = new System.Drawing.Point(148, 72);
+            this.columnName.Name = "columnName";
+            this.columnName.Size = new System.Drawing.Size(222, 26);
+            this.columnName.TabIndex = 2;
+            this.columnName.KeyDown += new System.Windows.Forms.KeyEventHandler(this.columnName_KeyDown);
+            // 
+            // addColumn
+            // 
+            this.addColumn.Location = new System.Drawing.Point(362, 123);
+            this.addColumn.Name = "addColumn";
+            this.addColumn.Size = new System.Drawing.Size(102, 38);
+            this.addColumn.TabIndex = 3;
+            this.addColumn.Text = "Add";
+            this.addColumn.UseVisualStyleBackColor = true;
+            this.addColumn.Click += new System.EventHandler(this.addColumn_Click);
+            // 
+            // txtClmnName
+            // 
+            this.txtClmnName.Location = new System.Drawing.Point(22, 75);
+            this.txtClmnName.Name = "txtClmnName";
+            this.txtClmnName.Size = new System.Drawing.Size(112, 24);
+            this.txtClmnName.TabIndex = 3;
+            this.txtClmnName.Text = "Column name";
+            // 
+            // btnDone
+            // 
+            this.btnDone.Location = new System.Drawing.Point(362, 249);
+            this.btnDone.Name = "btnDone";
+            this.btnDone.Size = new System.Drawing.Size(102, 38);
+            this.btnDone.TabIndex = 4;
+            this.btnDone.Text = "Done";
+            this.btnDone.UseVisualStyleBackColor = true;
+            this.btnDone.Click += new System.EventHandler(this.btnDone_Click);
+            // 
+            // deleteColumn
+            // 
+            this.deleteColumn.Location = new System.Drawing.Point(362, 167);
+            this.deleteColumn.Name = "deleteColumn";
+            this.deleteColumn.Size = new System.Drawing.Size(102, 39);
+            this.deleteColumn.TabIndex = 6;
+            this.deleteColumn.Text = "Delete";
+            this.deleteColumn.UseVisualStyleBackColor = true;
+            this.deleteColumn.Click += new System.EventHandler(this.deleteColumn_Click);
+            // 
+            // txtEntityname
+            // 
+            this.txtEntityname.Location = new System.Drawing.Point(22, 30);
+            this.txtEntityname.Name = "txtEntityname";
+            this.txtEntityname.Size = new System.Drawing.Size(112, 23);
+            this.txtEntityname.TabIndex = 6;
+            this.txtEntityname.Text = "Entity Name";
+            // 
+            // entityName
+            // 
+            this.entityName.Location = new System.Drawing.Point(148, 27);
+            this.entityName.MaxLength = 30;
+            this.entityName.Name = "entityName";
+            this.entityName.Size = new System.Drawing.Size(222, 26);
+            this.entityName.TabIndex = 1;
+            // 
+            // XML_GUI_NewTable
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 317);
+            this.Controls.Add(this.entityName);
+            this.Controls.Add(this.txtEntityname);
+            this.Controls.Add(this.deleteColumn);
+            this.Controls.Add(this.btnDone);
+            this.Controls.Add(this.txtClmnName);
+            this.Controls.Add(this.addColumn);
+            this.Controls.Add(this.columnName);
+            this.Controls.Add(this.columnsList);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon) (resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(515, 357);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(515, 357);
+            this.Name = "XML_GUI_NewTable";
+            this.Text = "New Document";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button addColumn;
+        private System.Windows.Forms.Button btnDone;
+        private System.Windows.Forms.TextBox columnName;
+        private System.Windows.Forms.ListBox columnsList;
+        private System.Windows.Forms.Button deleteColumn;
+        private System.Windows.Forms.TextBox entityName;
+        private System.Windows.Forms.Label txtClmnName;
+        private System.Windows.Forms.Label txtEntityname;
+
+        #endregion
+    }
+}
